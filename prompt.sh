423f551c946d66779575f8ba08937d2bbcd48863
Ryan Parman <ryan@ryanparman.com>
2015-06-01 00:38:06 -0700

    Resolved error message for new local-only branches
    Resolved an issue where new local-only branches would kick-back an unhelpful error message until pushed to the remote. These branches are now simply marked as `<local>` by default. Now has a color profile for people using the OS X Terminal’s "Basic" theme, which is the default.

--- prompt.sh
+++ prompt.sh
@@ -11,6 +11,7 @@ export GIT_EXTENDED_PROMPT_AHEAD="+"
 export GIT_EXTENDED_PROMPT_BEHIND="-"
 export GIT_EXTENDED_PROMPT_OK="\xE2\x9c\x93"
 export GIT_EXTENDED_PROMPT_NOK="\xE2\x80\xBC"
+export GIT_EXTENDED_PROMPT_NOREMOTE="<local>"
 
 # Lookup the current git branch, if any
 __wp_git_branch_lookup() {
@@ -58,15 +59,19 @@ __wp_git_branch_lookup() {
       fi
 
       # get the revision list, and count the leading "<" and ">"
-      revgit=`git rev-list --left-right ${remote_ref}...HEAD`
-      num_revs=`__wp_all_lines "$revgit"`
-      num_ahead=`__wp_count_lines "$revgit" "^>"`
-      num_behind=$(( num_revs - num_ahead ))
-      if (( num_behind > 0 )) ; then
-        remote="${remote}${symbols_behind}${num_behind}"
-      fi
-      if (( num_ahead > 0 )) ; then
-        remote="${remote}${symbols_ahead}${num_ahead}"
+      revgit=`git rev-list --left-right ${remote_ref}...HEAD 2> /dev/null`
+      if [[ $? = 0 ]]; then
+        num_revs=`__wp_all_lines "$revgit"`
+        num_ahead=`__wp_count_lines "$revgit" "^>"`
+        num_behind=$(( num_revs - num_ahead ))
+        if (( num_behind > 0 )) ; then
+          remote="${remote}${symbols_behind}${num_behind}"
+        fi
+        if (( num_ahead > 0 )) ; then
+          remote="${remote}${symbols_ahead}${num_ahead}"
+        fi
+      else
+        noremote=" $GIT_EXTENDED_PROMPT_NOREMOTE"
       fi
     fi
     if [[ -z "$remote" ]] ; then
@@ -125,9 +130,9 @@ __wp_git_branch_lookup() {
         untracked=""
       fi
 
-      echo -ne " ($branch$num_ahead$num_behind$conflicted$staged$changed$untracked)";
+      echo -ne "($branch$num_ahead$num_behind$conflicted$staged$changed$untracked$noremote)";
     else
-      echo -ne " ($branch)";
+      echo -ne "($branch)";
     fi
   else
     echo -ne "";
@@ -152,14 +157,36 @@ __wp_pass_fail() {
   fi
 }
 
-__wp_set_prompt_command() {
+__wp_set_prompt_command_for_dark() {
   # Build prompt in reverse so that we can properly capture the last exit code
   export PS1="\[$fg_bwhite\]`if [ $? = 0 ]; then echo -e \"\[$bg_green\] $GIT_EXTENDED_PROMPT_OK \"; else echo -e \"\[$bg_red\] $GIT_EXTENDED_PROMPT_NOK \"; fi`\[$reset\] "
   export PS1="\[$fg_byellow\]\$(__wp_git_branch_lookup)\[$reset\] $PS1" # Git status
-  export PS1="[\[$fg_bgreen\]\u@\h\[$reset\]: \[$fg_white\]\[$bg_blue\] \w \[$reset\]]$PS1" # User, hostname, current path
+  export PS1="\[$fg_bblack\]\T\[$reset\] $PS1" # Time
+  export PS1="[\[$fg_bgreen\]\u@\h\[$reset\]: \[$fg_white\]\[$bg_blue\] \w \[$reset\]] $PS1" # User, hostname, current path
+}
+
+__wp_set_prompt_command_for_basic() {
+  # Build prompt in reverse so that we can properly capture the last exit code
+  export PS1="\[$fg_bwhite\]`if [ $? = 0 ]; then echo -e \"\[$bg_green\] $GIT_EXTENDED_PROMPT_OK \"; else echo -e \"\[$bg_red\] $GIT_EXTENDED_PROMPT_NOK \"; fi`\[$reset\] "
+  export PS1="\[$fg_green\]\$(__wp_git_branch_lookup)\[$reset\] $PS1" # Git status
+  export PS1="\[$fg_bblack\]\T\[$reset\] $PS1" # Time
+  export PS1="[\[$fg_black\]\u@\h\[$reset\]: \[$fg_blue\]\w\[$reset\]] $PS1" # User, hostname, current path
 }
 
 # Always run on new prompt
-export PROMPT_COMMAND="__wp_set_prompt_command";
+export PROMPT_COMMAND="__wp_set_prompt_command_for_dark";
+
+#------------------------------------------------------------------------------#
 
 shopt -s checkwinsize
+
+# Tell the terminal about the working directory at each prompt.
+if [ "$TERM_PROGRAM" == "Apple_Terminal" ] && [ -z "$INSIDE_EMACS" ]; then
+  update_terminal_cwd() {
+      local SEARCH=' '
+      local REPLACE='%20'
+      local PWD_URL="file://$HOSTNAME${PWD//$SEARCH/$REPLACE}"
+      printf '\e]7;%s\a' "$PWD_URL"
+    }
+    PROMPT_COMMAND="$PROMPT_COMMAND; update_terminal_cwd"
+fi
